7803d2df6a8e2ec008608bbff7b7b85373afc8fc
Olga Filipova <olga.filipova@feedzai.com>
2013-09-30 17:57:22 +0100

    small refactor and added mouseenter and mouseleave events to the square to control resize class.

--- background.js
+++ background.js
@@ -1,33 +1,4 @@
-function onClickHandler(info) {
-    var id;
-
-    id = info.menuItemId;
-
-    switch(id) {
-        case 'square':
-            sendMessage('drawSquare', info);
-            break;
-        case 'capture':
-            captureScreen();
-            break;
-    }
-}
-
-function sendMessage(msgId, info) {
-    var objToSend = {msgId: msgId};
-
-    $.extend(true, objToSend, info);
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, objToSend);
-    });
-}
-
-function captureScreen() {
-    chrome.tabs.captureVisibleTab({format: 'png'}, function (imgData) {
-        sendMessage('saveImg', {imgData: imgData})
-    });
-}
-
+// Event handlers
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // Set up context menu tree at install time.
@@ -44,5 +15,48 @@ chrome.runtime.onInstalled.addListener(function() {
         "title": "Capture!",
         "id": "capture"
     });
-
 });
+
+/**
+ * Handles click on context menu
+ *
+ * @param {Object} info
+ */
+function onClickHandler(info) {
+    var id;
+
+    id = info.menuItemId;
+
+    switch(id) {
+        case 'square':
+            sendMessage('drawSquare', info);
+            break;
+        case 'capture':
+            captureScreen();
+            break;
+    }
+}
+
+/**
+ * Sends a message to script.js
+ *
+ * @param {String} msgId
+ * @param {Object} info
+ */
+function sendMessage(msgId, info) {
+    var objToSend = {msgId: msgId};
+
+    $.extend(true, objToSend, info);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, objToSend);
+    });
+}
+
+/**
+ * Captures a screen and send a message with captured data
+ */
+function captureScreen() {
+    chrome.tabs.captureVisibleTab({format: 'png'}, function (imgData) {
+        sendMessage('saveImg', {imgData: imgData})
+    });
+}
--- script.js
+++ script.js
@@ -1,55 +1,74 @@
-var position = {
-    top: '50%',
-    left: '50%'
-};
-
-function drawSquare() {
-    $('<div>')
-        .attr('id','screenshothelper')
-        .appendTo('body')
-        .css({
-            'background': 'transparent',
-            'position': 'absolute',
-            'border': '2px solid red',
-            'width': '200px',
-            'height': '200px',
-            'z-index': 3000,
-            'top': position.top,
-            'left': position.left
-        })
-        .resizable()
-        .draggable()
-        .dblclick(function () {
-            $(this).remove();
-        })
-        .find('.ui-icon.ui-icon-gripsmall-diagonal-se')
-        .removeClass('ui-icon ui-icon-gripsmall-diagonal-se');
-}
-
-function saveImage(imgData) {
-    window.open(imgData);
-}
-
 $(document).ready(function () {
+    // Globals
+    var position = {
+        top: '50%',
+        left: '50%'
+    };
+
+    // Event handlers
+    /**
+     * On contextmenu click save the mouse position
+     */
     $(document).on('contextmenu', function (ev) {
         position = {
             top: ev.pageY,
             left: ev.pageX
         }
     });
-});
+    /**
+     * On message received analyze a request and call the appropriate method
+     */
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        var msgId;
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    var msgId;
+        msgId = request.msgId;
 
-    msgId = request.msgId;
+        switch (msgId) {
+            case 'drawSquare':
+                drawSquare();
+                break;
+            case 'saveImg':
+                saveImage(request.imgData);
+                break;
+        }
+    });
+    /**
+     * Draws draggable resizable transparent red square on the page
+     */
+    function drawSquare() {
+        $('<div>')
+            .addClass('screenshothelper')
+            .appendTo('body')
+            .css({
+                'background': 'transparent',
+                'position': 'absolute',
+                'border': '2px solid red',
+                'width': '200px',
+                'height': '200px',
+                'z-index': 3000,
+                'top': position.top,
+                'left': position.left
+            })
+            .resizable()
+            .draggable()
+            .dblclick(function () {
+                $(this).remove();
+            })
+            .mouseenter(function () {
+                $(this).find('.ui-resizable-se')
+                    .addClass('ui-icon ui-icon-gripsmall-diagonal-se');
+            }).mouseleave(function () {
+                $(this).find('.ui-resizable-se')
+                    .removeClass('ui-icon ui-icon-gripsmall-diagonal-se');
+            });
+    }
 
-    switch (msgId) {
-        case 'drawSquare':
-            drawSquare();
-            break;
-        case 'saveImg':
-            saveImage(request.imgData);
-
+    /**
+     * Opens a new page with the image
+     *
+     * @param {string} imgData
+     */
+    function saveImage(imgData) {
+        window.open(imgData);
     }
 });
